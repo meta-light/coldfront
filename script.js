7fe5cddef846c7a0ffbf65ee05abb23178621450
Michael Stang <mikestang808@gmail.com>
2023-03-07 11:34:51 -0600

    my commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY ='ea9ca8e1ebcf9a570280af6f0f984612';
+const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
 
 setInterval(() => {
     const time = new Date();
