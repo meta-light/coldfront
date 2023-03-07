52849adc671eb645273ef9863ef513ed4f5dc859
Michael Stang <mikestang808@gmail.com>
2023-03-07 12:18:54 -0600

    sprint 2 final -2

--- script.js
+++ script.js
@@ -64,11 +64,11 @@ function showWeatherData (data){
     </div>
     <div class="weather-item">
         <div>Sunrise</div>
-        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+        <div>${window.moment(sunrise * 1000).format('H:mm a')}</div>
     </div>
     <div class="weather-item">
         <div>Sunset</div>
-        <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
+        <div>${window.moment(sunset*1000).format('H:mm a')}</div>
     </div>
     
     
