f705b55261258660b0b3a0304db8b29ad7c4bb92
meta-lite <nick@myceliumnetworks.com>
2023-04-04 14:48:18 -0500

    Correct rain API call and functions

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
-
-
+const currentPredictionEl = document.getElementById('current-prediction');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
 const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
 
 setInterval(() => {
@@ -21,34 +19,46 @@ setInterval(() => {
     const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
     const minutes = time.getMinutes();
     const ampm = hour >=12 ? 'PM' : 'AM'
-
     timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
-
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
-
 }, 1000);
 
 getWeatherData()
 function getWeatherData () {
     navigator.geolocation.getCurrentPosition((success) => {
-        
         let { latitude, longitude } = success.coords;
-
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`).then(res => res.json()).then(data => {
-
         console.log(data)
         showWeatherData(data);
         })
-
     })
+}
+
+getPrediction()
+function getPrediction() {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=36.0647&lon=-94.1744&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+        const rainChance = data.daily[0].rain ? `${data.daily[0].rain} inches` : '0 inches';
+        console.log(`The chance of rain in Fayetteville, AR is ${rainChance}.`);
+        showPrediction(data);
+    })
+    .catch(error => console.error(error));
+}
+
+function showPrediction (data){
+    let rainChance = data.daily[0].rain ? `${data.daily[0].rain} inches` : '0 inches';
+    currentPredictionEl.innerHTML = 
+    `<div class="weather-item">
+        <div>Rain Chance</div>
+        <div>${rainChance}</div>
+    </div>`;
 }
 
 function showWeatherData (data){
     let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
-
     timezone.innerHTML = data.timezone;
     countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
-
     currentWeatherItemsEl.innerHTML = 
     `<div class="weather-item">
         <div>Humidity</div>
@@ -69,10 +79,7 @@ function showWeatherData (data){
     <div class="weather-item">
         <div>Sunset</div>
         <div>${window.moment(sunset*1000).format('H:mm a')}</div>
-    </div>
-    
-    
-    `;
+    </div>`;
 
     let otherDayForcast = ''
     data.daily.forEach((day, idx) => {
@@ -83,9 +90,7 @@ function showWeatherData (data){
                 <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
                 <div class="temp">Night - ${day.temp.night}&#176;F</div>
                 <div class="temp">Day - ${day.temp.day}&#176;F</div>
-            </div>
-            
-            `
+            </div>`
         }else{
             otherDayForcast += `
             <div class="weather-forecast-item">
@@ -93,12 +98,8 @@ function showWeatherData (data){
                 <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
                 <div class="temp">Night - ${day.temp.night}&#176;F</div>
                 <div class="temp">Day - ${day.temp.day}&#176;F</div>
-            </div>
-            
-            `
+            </div>`
         }
     })
-
-
     weatherForecastEl.innerHTML = otherDayForcast;
 }
