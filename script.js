c3ed0b496d0b2d8404a47bae92f0e6998c327855
Michael Stang <mikestang808@gmail.com>
2023-04-17 19:49:43 -0500

    game status emojis, quick fix for better UI&UX is adding 'scrollstyle.css' (not permanent), & making hamburger faster onclick

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function getPrediction() {
     .then(res => res.json())
     .then(data => {
         const rainAmount = data.daily[0].rain ? data.daily[0].rain : 0;
-        const willCancel = rainAmount >= 0.5 ? '✅' : '🚫';
+        const willCancel = rainAmount >= 0.5 ? '🚫' : '✅';
         console.log(`Will the baseball game be cancelled due to rain in Fayetteville, AR? ${willCancel}`);
         showPrediction(data, willCancel);
     })
