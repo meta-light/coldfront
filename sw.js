688dc9b7e41743d3140ac20de883f63d2ca283e9
Michael Stang <mikestang808@gmail.com>
2023-05-08 20:45:17 -0500

    trial of offline availablity in PWA

--- sw.js
+++ sw.js
@@ -1,15 +1,30 @@
 self.addEventListener("install", e => {
-    e.waitUnitl(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./style.css", "./media/profile144.jpg"]);
-        })
+    e.waitUntil(
+      caches.open("static").then(cache => {
+        return cache.addAll(["./", "./style.css", "./media/profile144.jpg"]);
+      })
     );
-});
-
-self.addEventListener("fetch", e => {
+  });
+  
+  self.addEventListener("fetch", e => {
     e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
+      caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+      })
     );
-});
+  });
+  
+  self.addEventListener("activate", e => {
+    e.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.filter(cacheName => {
+            return cacheName.startsWith("static") && cacheName !== "static";
+          }).map(cacheName => {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
+  
